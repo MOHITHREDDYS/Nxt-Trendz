bfbce290a6bffe4f9c80179cf717571a8b0e9ab6
MOHITHREDDYS <mohithreddys.531@gmail.com>
2022-12-11 10:06:01 +0000

    link component  added to similar items

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,31 +1,34 @@
+import {Link, withRouter} from 'react-router-dom'
 import './index.css'
 
 const SimilarProductItem = props => {
   const {productDetails} = props
-  const {title, brand, imageUrl, rating, price} = productDetails
+  const {id, title, brand, imageUrl, rating, price} = productDetails
 
   return (
     <li className="similar-product-item">
-      <img
-        src={imageUrl}
-        className="similar-product-img"
-        alt={`similar product ${title}`}
-      />
-      <p className="similar-product-title">{title}</p>
-      <p className="similar-products-brand">by {brand}</p>
-      <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rs {price}/-</p>
-        <div className="similar-product-rating-container">
-          <p className="similar-product-rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="similar-product-star"
-          />
+      <Link to={`/products/${id}`} className="link-item">
+        <img
+          src={imageUrl}
+          className="similar-product-img"
+          alt={`similar product ${title}`}
+        />
+        <p className="similar-product-title">{title}</p>
+        <p className="similar-products-brand">by {brand}</p>
+        <div className="similar-product-price-rating-container">
+          <p className="similar-product-price">Rs {price}/-</p>
+          <div className="similar-product-rating-container">
+            <p className="similar-product-rating">{rating}</p>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+              alt="star"
+              className="similar-product-star"
+            />
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   )
 }
 
-export default SimilarProductItem
+export default withRouter(SimilarProductItem)
